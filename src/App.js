3bd0d13a122c3dcd70ad916349e7559e87942c79
Erin Dawson <erindawson@DAGA-3763.local>
2018-01-05 11:48:48 -0500

    placeholders for app.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src="https://aaronopia.files.wordpress.com/2016/11/goddess-lady-justice-statue.jpg" className="App-logo" alt="logo" />
           <h1 className="App-title">1881 Initiative</h1>
+          <h3>Committed to Lady AGs</h3>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          The initiative, co-chaired by Massachusetts Attorney General Maura Healey and Oregon Attorney General Ellen Rosenblum, sets the goal of electing women to 50 percent of the democratically held state attorney general seats by the end of 2022 and honors the history of women running for the office. The program aims to attract and support the most qualified and talented female candidates through trainings, events and serving as a resource for women considering running for the state office of attorney general.
         </p>
       </div>
     );
