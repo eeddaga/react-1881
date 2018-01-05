bb2f7139d7eee2e9972230d93194ab985bcb269d
Erin Dawson <erindawson@DAGA-3763.local>
2018-01-05 12:51:17 -0500

    add boostrap cdns.  create NavContainer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import NavContainer from './NavContainer.js';
 import './App.css';
+
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
           <img src="https://aaronopia.files.wordpress.com/2016/11/goddess-lady-justice-statue.jpg" className="App-logo" alt="logo" />
           <h1 className="App-title">1881 Initiative</h1>
           <h3>Committed to Lady AGs</h3>
+            <NavContainer />
         </header>
         <p className="App-intro">
           The initiative, co-chaired by Massachusetts Attorney General Maura Healey and Oregon Attorney General Ellen Rosenblum, sets the goal of electing women to 50 percent of the democratically held state attorney general seats by the end of 2022 and honors the history of women running for the office. The program aims to attract and support the most qualified and talented female candidates through trainings, events and serving as a resource for women considering running for the state office of attorney general.
---
+++ src/NavContainer.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './NavContainer.css';
+
+
+class NavContainer extends Component {
+  render (){
+    return (
+      <ul>
+      <li>About Us</li>
+      <li>Resources</li>
+      <li>Get Involved</li>
+      <li>Donate</li>
+      </ul>
+    )
+  }
+}
+
+
+export default NavContainer;
