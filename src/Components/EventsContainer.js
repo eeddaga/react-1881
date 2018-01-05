02a44190a7ce49a13acfd30d530dcd92be47630a
Erin Dawson <erindawson@DAGA-3763.local>
2018-01-05 17:15:57 -0500

    add email field to EventsContainer

--- src/Components/EventsContainer.js
+++ src/Components/EventsContainer.js
@@ -3,7 +3,17 @@ import React, {Component} from 'react';
 class EventsContainer extends Component {
   render (){
     return (
+      <div>
       <h1>Calendar of Events</h1>
+
+
+      <form>
+        <label>Email:</label>
+        <input type="text"/>
+      </form>
+      </div>
+
+
     )
   }
 }
