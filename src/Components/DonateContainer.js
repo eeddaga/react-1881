e4a6fa1c37680129868af87f891d1df3ef5ba6d6
Erin Dawson <erindawson@DAGA-3763.local>
2018-01-08 10:15:56 -0500

    add Donate link to bsd page

--- src/Components/DonateContainer.js
+++ src/Components/DonateContainer.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class DonateContainer extends Component {
   render(){
     return (
-      <h2>pizza fund</h2>
+      <h3><a href="https://secure.actblue.com/donate/democratic-attorneys-general-association-1" target="_blank">Yes, I want more women in the role of Attorney General!</a></h3>
     )
   }
 }
