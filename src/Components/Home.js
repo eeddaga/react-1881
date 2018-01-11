434c992ce20e06aaf8dca9201bda3c5182a2a44b
Erin Dawson <erindawson@DAGA-3763.local>
2018-01-11 12:09:11 -0500

    complete styling and functionality for Home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
+import './Home.css';
 
 
 
 class Home extends Component {
   render(){
     return(
-      <div className="hero">
-      <img src="/liberty.jpg" className="hero" />
-        <div className="hero-text">
-        Austin waistcoat bitters raclette asymmetrical prism next level, <br/>single-origin coffee thundercats biodiesel readymade forage selfies
+      <div className="home-container">
+        <div className="home-container-text">
+        <h3>Austin waistcoat bitters raclette asymmetrical prism next level, <br/>single-origin coffee thundercats biodiesel readymade forage selfies</h3>
+        <p>Waistcoat migas XOXO coloring book banh mi, seitan fanny pack farm-to-table meditation vegan banjo disrupt. Intelligentsia ugh XOXO shoreditch kinfolk jianbing fam, pug poke kickstarter tote bag asymmetrical. Shabby chic green juice small batch humblebrag. </p>
         </div>
       </div>
 
